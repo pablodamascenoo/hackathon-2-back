83154293c960a17d75bb1caca006b971882a3fbd
pablodamascenoo <pablofacex@gmail.com>
2022-05-20 19:35:03 -0300

    fix: fixed routes problem

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { postRegister, postLogin } from ".././controllers/authController.js"
-import { validateRegister, loginSchema } from "../middlewares/authMiddleware.js";
+import { postRegister, postLogin } from ".././controllers/authController.js";
+import {
+    validateRegister,
+    loginSchema,
+} from "../middlewares/authMiddleware.js";
 
 const authRouter = Router();
 
-authRouter.post('/sign-up', validateRegister, postRegister);
-authRouter.post('/sign-in', loginSchema, postLogin);
+authRouter.post("/sign-up", validateRegister, postRegister);
+authRouter.post("/sign-in", loginSchema, postLogin);
 
 export default authRouter;
